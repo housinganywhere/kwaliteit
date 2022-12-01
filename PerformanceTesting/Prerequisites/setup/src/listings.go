9315071bf8029a415f5cbd419839e0cd4eec04be
zeek-r <r.bista@housinganywhere.com>
2022-12-01 11:07:26 +0100

    feat(listings-concurrency): array equal to count

--- PerformanceTesting/Prerequisites/setup/src/listings.go
+++ PerformanceTesting/Prerequisites/setup/src/listings.go
@@ -4,41 +4,58 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
 
-func createListing(client *http.Client, listingDetailsBytesArray []byte) map[string]string {
-	resp, err := client.Post(host+"/api/v2/listing", "application/json",
+func createListing(client *http.Client, listingDetailsBytesArray []byte) (map[string]string, error) {
+	path := "/api/v2/listing"
+
+	resp, err := client.Post(fmt.Sprintf("%s%s", host, path), "application/json",
 		bytes.NewBuffer(listingDetailsBytesArray))
+
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("error: %v, path: %s, statusCode: %d", err, path, resp.StatusCode)
 	}
+
 	if resp.StatusCode != 200 {
-		log.Fatal("/api/v2/listing request returned error")
+		body, _ := ioutil.ReadAll(resp.Body)
+		return nil, fmt.Errorf("error: %v, path: %s, statusCode: %d", string(body), path, resp.StatusCode)
 	}
+
 	var res map[string]interface{}
 	json.NewDecoder(resp.Body).Decode(&res)
 
 	listingId := strings.Split(res["listingPath"].(string), "/")[2]
 
-	req, err := http.NewRequest(http.MethodPut, host+"/api/v2/listing/"+listingId+"/publish", bytes.NewBuffer([]byte{}))
+	path = fmt.Sprintf("%s/%s%s", path, listingId, "/publish")
+
+	req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("%s%s", host, path), bytes.NewBuffer([]byte{}))
 	req.Header.Add("Content-Type", "application/json")
+
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	resp1, _ := client.Do(req)
+
+	resp1, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
 
 	var res1 map[string]interface{}
 	json.NewDecoder(resp1.Body).Decode(&res1)
+
 	if resp1.StatusCode != 204 {
-		log.Fatal("/api/v2/user/ request returned error")
+		body, _ := ioutil.ReadAll(resp.Body)
+		return nil, fmt.Errorf("error: %v, path: %s, statusCode: %d", string(body), path, resp.StatusCode)
+
 	}
 	listingDetails := map[string]string{}
 	listingDetails["listingId"] = listingId
 	listingDetails["listingUuid"] = res["uuid"].(string)
 	listingDetails["advertiserId"] = strings.Split(fmt.Sprintf("%f", res["advertiserId"]), ".")[0]
 
-	return listingDetails
+	return listingDetails, nil
 }
